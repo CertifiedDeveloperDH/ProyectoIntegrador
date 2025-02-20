c2d6e0ca19c3232e028593e7facc00b3b7710663
certifieddeveloper085 <certifieddeveloper085@gmail.com>
2025-02-20 16:03:02 -0300

    proyecto integrador

--- api-gateway/api-gateway/src/main/java/com/example/api_gateway/security/GatewaySecurityConfig.java
+++ api-gateway/api-gateway/src/main/java/com/example/api_gateway/security/GatewaySecurityConfig.java
@@ -3,11 +3,13 @@ package com.example.api_gateway.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class GatewaySecurityConfig {
 
     @Bean
