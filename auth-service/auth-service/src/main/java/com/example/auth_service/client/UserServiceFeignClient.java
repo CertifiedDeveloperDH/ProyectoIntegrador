fbed5a8ab5113f6490ac00ea7536db2694a99298
certifieddeveloper085 <certifieddeveloper085@gmail.com>
2025-02-25 14:12:18 -0300

    proyecto integrador

--- auth-service/auth-service/src/main/java/com/example/auth_service/client/UserServiceFeignClient.java
+++ auth-service/auth-service/src/main/java/com/example/auth_service/client/UserServiceFeignClient.java
@@ -2,15 +2,16 @@ package com.example.auth_service.client;
 
 import com.example.auth_service.model.User;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-@FeignClient(name = "user-service")
+@FeignClient(name = "user-service", url = "http://localhost:8087")
 public interface UserServiceFeignClient {
 
     @PostMapping("/users/register")
-    void registerUser(@RequestBody User user); // Acepta User ahora
+    ResponseEntity<String> registerUser(@RequestBody User user);
 
 }
 
